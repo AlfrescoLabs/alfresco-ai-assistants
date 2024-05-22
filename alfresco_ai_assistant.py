a538b9fbf7719575d25a0c3c9bd6f704868a0e6d
Domenico Sibilio <domenicosibilio@gmail.com>
2024-05-22 12:52:31 +0200

    Add tool to list recent snippets matching a search term (#8)

--- alfresco_ai_assistant.py
+++ alfresco_ai_assistant.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 from alfresco_api import *
 import streamlit as st
@@ -84,32 +85,6 @@ def get_document_content(document_title: str) -> dict:
     response = node_api.get_node_content(node_id)
     return {"document_title": document_title, "content": response}
 
-def copy_file_to_folder(filename: str, folder_name: str) -> dict:
-    search_response = search_api.search_folders_by_name(folder_name)
-    try:
-        folder_id = search_response["list"]["entries"][0]["entry"]["id"]
-    except IndexError:
-        return FOLDER_NOT_FOUND
-
-    search_response = search_api.search_by_name(filename)
-    try:
-        node_id = search_response["list"]["entries"][0]["entry"]["id"]
-    except IndexError:
-        return DOCUMENT_NOT_FOUND
-
-    response = node_api.copy_to_folder(node_id, folder_id)
-    try:
-        filename = response["entry"]["name"]
-        return {"copied_filename": filename}
-    except KeyError:
-        error = response["error"]
-        return {"error": error}
-
-@tool
-def multiply(first_int: int, second_int: int) -> int:
-    """Multiply two integers together."""
-    return first_int * second_int
-
 @tool
 def discovery() -> dict:
     """Discover the current Alfresco Content Services (ACS) version, installed modules, and license status."""
@@ -147,9 +122,49 @@ def redact_content(document_title: str, user_request: str) -> str:
 @tool
 def copy_file(filename: str, folder_name: str) -> dict:
     """Copy file within Alfresco Content Services (ACS) to the specified folder."""
-    return copy_file_to_folder(filename, folder_name)
+    folders = search_api.search_folders_by_name(folder_name)
+    try:
+        folder_id = folders["list"]["entries"][0]["entry"]["id"]
+        folder_name = folders["list"]["entries"][0]["entry"]["name"]
+    except IndexError:
+        return FOLDER_NOT_FOUND
 
-tools = [multiply, discovery, transform_content, translate_content, redact_content, copy_file]
+    documents = search_api.search_by_name(filename)
+    try:
+        node_id = documents["list"]["entries"][0]["entry"]["id"]
+    except IndexError:
+        return DOCUMENT_NOT_FOUND
+
+    response = node_api.copy_to_folder(node_id, folder_id)
+    try:
+        filename = response["entry"]["name"]
+        return {"successfully_copied_file": filename, "destination_folder": folder_name}
+    except KeyError:
+        error = response["error"]
+        return {"error": error}
+
+@tool
+def list_recent_content_snippets(search_term: str) -> dict:
+    """Find and show the snippets of recent documents within Alfresco Content Services (ACS) that contain a certain search term."""
+    response = search_api.search_recent_docs_snippets(search_term)
+    entries = response["list"]["entries"]
+    if not entries:
+        return f"No recent documents found containing the search term '{search_term}'."
+
+    # simplify the JSON response preserving only the relevant information
+    results = {}
+    for entry in entries:
+        entry = {
+            "file_name": entry["entry"]["name"],
+            "created_date": entry["entry"]["createdAt"],
+            "edited_date": entry["entry"]["modifiedAt"],
+            "highlight_snippets": entry["entry"]["search"]["highlight"][0]["snippets"]
+        }
+        results[f"{entry['file_name']}_{uuid.uuid4()}"] = entry
+
+    return results
+
+tools = [discovery, transform_content, translate_content, redact_content, list_recent_content_snippets, copy_file]
 rendered_tools = render_text_description(tools)
 
 system_prompt = f"""You are a robot that only outputs JSON, and has access to the following set of tools. Here are the names and descriptions for each tool:
@@ -164,11 +179,12 @@ Do not include anything before or after the JSON blob. Do not mention that you a
 
 prompt_messages = [("system", system_prompt)]
 example_messages = [
-    ("user", "Multiply 12 by 43"), ("assistant", '{{"name": "multiply", "arguments": {{"first_int": 12, "second_int": 43}}}}'),
     ("user", "Is the Alfresco Content Services license up to date?"), ("assistant", '{{"name": "discovery", "arguments": {{}}}}'),
     ("user", "Summarise the content of the document titled 'minutes.docx'"), ("assistant", '{{"name": "transform_content", "arguments": {{"document_title": "minutes.docx"}}}}'),
     ("user", "Translate the content of the document titled 'minutes.docx' to French"), ("assistant", '{{"name": "translate_content", "arguments": {{"document_title": "minutes.docx", "language": "French"}}}}'),
     ("user", "Redact all mentions of colors and names in 'snowwhite.docx'"), ("assistant", '{{"name": "redact_content", "arguments": {{"document_title": "snowwhite.docx", "user_request": "colors, names"}}}}'),
+    ("user", "Show snippets of recent documents that contain the term 'contract'"), ("assistant", '{{"name": "list_recent_content_snippets", "arguments": {{"search_term": "contract"}}}}'),
+    ("user", "Copy 'minutes.docx' to the 'Board Meetings' folder"), ("assistant", '{{"name": "copy_file", "arguments": {{"filename": "minutes.docx", "folder_name": "Board Meetings"}}}}'),
 ]
 prompt_messages += example_messages
 prompt_messages.append(("user", "{input}"))
