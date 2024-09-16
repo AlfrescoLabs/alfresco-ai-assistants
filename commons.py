25535efa5478e1751a71930d059587ac3055cbac
Domenico Sibilio <domenicosibilio@gmail.com>
2024-09-16 13:25:15 +0200

    Restore num_ctx

--- commons.py
+++ commons.py
@@ -72,7 +72,7 @@ def load_llm(llm_name: str, logger=BaseLogger(), config={}):
             # seed=2,
             top_k=10,  # A higher value (100) will give more diverse answers, while a lower value (10) will be more conservative.
             top_p=0.3,  # Higher value (0.95) will lead to more diverse text, while a lower value (0.5) will generate more focused text.
-            num_ctx=3072,  # Sets the size of the context window used to generate the next token.
+            num_ctx=32768,  # Sets the size of the context window used to generate the next token.
         )
     logger.info("LLM: Using GPT-3.5")
     return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
